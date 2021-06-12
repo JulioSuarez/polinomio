4ffe1c2dc5305a17f00431a9fd4660ed55ed8b0c
JulioSuarez <60273721+JulioSuarez@users.noreply.github.com>
2021-06-12 15:52:40 -0400

    Eliminar comentarios
    	// object{
    	// string: name
    	// int number
    	// }
    	//
    	// object xx = new object();
    	// xx.name = "edd"
    	// xx.number=123
    	//
    	// object yy;
    	// yy.name="123"
    	// yy.number=444

--- JCSTPoliV.h
+++ JCSTPoliV.h
@@ -1,7 +1,24 @@
-//---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 
 #ifndef JCSTPoliVH
 #define JCSTPoliVH
-//---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
+#include <iostream>
+#include <string>
+using namespace std;
 
+const int MAXP = 100;
+
+class JCSTPoliV {
+
+private:
+	int* vc; // int vc[MAXP];
+	int* ve; // int ve[MAXP];
+	int nt;
+
+public:
+	JCSTPoliV();
+	JCSTPoliV(int nrot);
+	bool es_cero();
+};
 #endif
--- main.cpp
+++ main.cpp
@@ -4,13 +4,40 @@
 #ifdef _WIN32
 #include <tchar.h>
 #else
-  typedef char _TCHAR;
-  #define _tmain main
+typedef char _TCHAR;
+#define _tmain main
 #endif
 
 #include <stdio.h>
+#include "JCSTPoliV.h"
 
- int _tmain(int argc, _TCHAR* argv[]) 
-{
+int main() {
+	// object{
+	// string: name
+	// int number
+	// }
+	//
+	// object xx = new object();
+	// xx.name = "edd"
+	// xx.number=123
+	//
+	// object yy;
+	// yy.name="123"
+	// yy.number=444
+
+	JCSTPoliV* poliv = new JCSTPoliV(); //opcin 1
+
+
+//	bool result = poliv.es_cero();
+//	if (result) {
+//		cout<<"verdad"<<endl;
+//	}
+//	else {
+//		cout<<"falso"<<endl;
+//
+//	}
+	system("pause");
+
+
 	return 0;
 }
